3e9544b449392ae412aeb72ce6b8c0eb1bba2dc4
damoski2 <akogunoyindamola42@gmail.com>
2022-06-06 02:33:59 +0100

    minor fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,14 @@
-import '../styles/globals.css'
-import { Header, Footer } from '../components/imports'
-import { ThemeProvider } from "next-themes";
-
+import "../styles/globals.css";
+import { Header, Footer } from "../components/imports";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
+    <>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </ThemeProvider>
-  )
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
